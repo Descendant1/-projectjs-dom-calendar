e4abb2a5de551d2557bfc60dee232272c4c833c0
pavelkyrnats <33157251+Descendant1@users.noreply.github.com>
2019-02-11 01:44:34 +0200

    Implemented first two pages;

---
+++ DOMlib.js
@@ -0,0 +1,117 @@
+class library {
+
+    constructor(selector){
+        this.currEl = this.get(selector);
+    }
+
+    get = ( selector ) => { 
+
+        if( ! ( typeof selector === 'string' || selector instanceof String ) ){
+            return;
+        }
+
+        if( selector[0] === '#' ){
+            this.currEl = document.querySelector( selector );
+            return  this.currEl;
+        }
+
+        return Array.from( document.querySelectorAll( selector ) );
+    }
+
+    remove = (  ) => {
+        var el = this.currEl;
+        Array.isArray( el ) ? el.map( i => i.remove() ) : el.remove();
+        this.currEl= null;
+    }
+
+    createElement = ( tagName, props ) => 
+    {
+        return this.setAttrsToElement( document.createElement( tagName ), props);
+    }
+
+    append= ( tagName, props ) => {
+        if( tagName == undefined || tagName == null ){
+            console.log('%c TagName invalid! ', ' color: red');
+            return;
+        }
+        this.currEl =   Array.isArray( this.currEl ) ? this.currEl.map( i =>  i.appendChild(  this.createElement( tagName, props ) ) )
+                            : this.currEl.appendChild( this.createElement( tagName, props ) );
+        return this;
+    }
+
+    modify = ( props ) => {
+        
+        Array.isArray( this.currEl ) ? this.currEl.map( i =>  this.setAttrsToElement( i, props ) ) 
+                                     : this.setAttrsToElement( this.currEl, props);  
+        return this;
+    }
+
+    modifyStyles = (  props ) => {
+        var   setStylesToElement =( target, props )=>{
+            for( var key in props ){
+                target.style[ key ] = props[ key ];
+            }            
+            return target;
+        }
+        Array.isArray( this.currEl ) ? this.currEl.map( i =>  setStylesToElement( i, props ) ) 
+                            : setStylesToElement( this.currEl, props);  
+    }
+
+    setAttrsToElement =( target, props )=>{
+        for( var key in props ){
+            if (key =='innerText'){
+                target.innerText = props[key]; 
+
+                continue;
+            }
+            else if (key =='innerHTML'){
+                target.innerHTML = props[key]; 
+                continue;
+            }
+            target.setAttribute( key, props[key] )
+        }
+        
+        return target;
+    }
+
+    getParent = () => {
+        this.currEl =  Array.isArray( this.currEl )  ? this.currEl.map( i => i.parentElement ) 
+                                                     : this.currEl.parentElement;  
+        return this;
+    }
+    getNextSibling = () => {
+        this.currEl =  Array.isArray( this.currEl )  ? this.currEl.map( i => i.nextSibling ) 
+                                                     : this.currEl.nextSibling;  
+        return this;
+    }
+    getPrevSibling = () => {
+        this.currEl =  Array.isArray( this.currEl )  ? this.currEl.map( i => i.previousSibling ) 
+                                                     : this.currEl.previousSibling;  
+        return this;
+    }
+    getChildren = () => {
+        this.currEl =  Array.isArray( this.currEl )  ? this.currEl.map( i => i.children ) 
+                                                     : this.currEl.children;  
+        return this;
+    }
+
+    setEventHandler = (props) => {
+        var setEventToEventHandler = ( target ,props ) => {
+            for( var key in props ){
+                target[key] = props[key] ;
+            }
+        }
+        Array.isArray( this.currEl )  ? this.currEl.map( i => setEventToEventHandler( i, props ) ) 
+                                                     : setEventToEventHandler(this.currEl , props);  
+
+        return this;
+    }
+}
+const $ = (sel) => { return new library(sel) }
+
+//var  e =  new ir();
+
+//(()=>{
+//    console.log(e)//console.log('%c Some Error! ', ' color: red');
+//})();
+
